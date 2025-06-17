0ce9d06a9096ae2825d362263ac25c5edab00951
HunEee <asdf981001@naver.com>
2025-06-17 23:09:28 +0900

    초기 세팅

---
+++ src/test/java/com/spring_security/TestSecurity/TestSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_security.TestSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
